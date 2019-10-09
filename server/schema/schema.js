89b235c45b24cc65fa3db800ed2c49339fa0cbfd
Damhan Richardson <damhanrichardson@gmail.com>
2019-10-09 22:13:58 +0100

    Created rootqueries for all books & authors.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -84,6 +84,18 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args) {
                 return _.find(authors, {id:args.id})
             }
+        },
+        books:{
+            type: GraphQLList(BookType),
+            resolve(parent,args) {
+                return books;
+            }
+        },
+        authors: {
+            type: GraphQLList(AuthorType),
+            resolve(parent,args) {
+                return authors;
+            }
         }
     }
 });
