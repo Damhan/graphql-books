59f729964e3c29f9fcc345cb95302bba80e751a8
Damhan Richardson <damhanrichardson@gmail.com>
2019-10-10 22:30:10 +0100

    Externalized queries, added State to AddBook component using React Hooks.

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import {gql} from 'apollo-boost';
+import {useState} from 'react'; 
 import {graphql} from 'react-apollo'; 
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`
+import {getAuthorsQuery} from './../queries/queries';
 
 const AddBook = props => {
+
+    const initialState = {
+        name:"",
+        genre:"",
+        authorId:""
+    }
+    //Set our state to default state before form is filled out.
+    const[book, setBook] = useState(initialState);
+
+    const submitBookForm = event => {
+        event.preventDefault()
+        console.log(book);
+        setBook(initialState);
+    }
+
+    const handleChange = event => {
+        const {name, value} = event.target
+        setBook({...book, [name]:value});
+
+    
+    }
+
 
     //Populate the select dropdown with author names.
     const populateAuthors = (props) => {
         var data = props.data;
         if(!data.loading) {
             return data.authors.map(author => {
-                return(<option key={author.id}>{author.name}</option>)
+                return(<option key={author.id} value={author.id}>{author.name}</option>)
             })
         }
         else{
@@ -27,19 +41,20 @@ const AddBook = props => {
     }
 
     return (
-      <form>
+      <form onSubmit={submitBookForm}>
           <div className="field">
               <label>Book name:</label>
-              <input type="text"/>
+              <input type="text" name="name" onChange={handleChange}/>
           </div>
           <div className="field">
               <label>Genre:</label>
-              <input type="text"/>
+              <input type="text" name="genre" onChange={handleChange}/>
           </div>
 
           <div className="field">
               <label>Author:</label>
-              <select>
+              <select name="authorId" onChange={handleChange}>
+                  <option >Select Author</option>
                   {populateAuthors(props)}
               </select>
           </div>
